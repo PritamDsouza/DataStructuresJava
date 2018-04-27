a4d7ce85721199b75b285191d22fd3dce4f273b1
PritamDsouza <30990020+PritamDsouza@users.noreply.github.com>
2018-04-27 01:30:48 -0500

    with proper indentation

--- src/DynamicStackProgram/DynamicStack.java
+++ src/DynamicStackProgram/DynamicStack.java
@@ -4,15 +4,15 @@ public class DynamicStack {
 	int capacity = 2;
 	int[] stack = new int[capacity];
 	int top = 0;
-	
+
 	public void push(int data) {
-		if(size() == capacity) {
+		if (size() == capacity) {
 			expand();
 		}
 		stack[top] = data;
 		top++;
 	}
-	
+
 	private void expand() {
 		int length = size();
 		int newStack[] = new int[capacity * 2];
@@ -20,14 +20,13 @@ public class DynamicStack {
 		stack = newStack;
 		capacity *= 2;
 	}
-	
+
 	public int pop() {
-		
+
 		int data = 0;
-		if(isEmpty()) {
+		if (isEmpty()) {
 			System.out.println("Stack is empty");
-		}
-		else {
+		} else {
 			top--;
 			data = stack[top];
 			stack[top] = 0;
@@ -35,34 +34,34 @@ public class DynamicStack {
 		}
 		return data;
 	}
-	
+
 	public void shrink() {
 		int length = size();
-		if(length <= (capacity/2)/2) {
+		if (length <= (capacity / 2) / 2) {
 			capacity /= 2;
 		}
 		int[] newStack = new int[capacity];
 		System.arraycopy(stack, 0, newStack, 0, length);
 		stack = newStack;
-		
+
 	}
-	
+
 	public int peek() {
 		int data;
 		data = stack[top - 1];
 		return data;
 	}
-	
+
 	public int size() {
 		return top;
 	}
-	
+
 	public boolean isEmpty() {
 		return top <= 0;
 	}
-	
+
 	public void show() {
-		for(int n: stack) {
+		for (int n : stack) {
 			System.out.print(n + " ");
 		}
 		System.out.println();
